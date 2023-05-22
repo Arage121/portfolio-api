e553c5cb9b18fb6e222a7ed540ecb936bd91328b
ayush <rajayush746@gmail.com>
2023-05-22 21:04:29 +0530

    change

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 // const fs = require('fs');
