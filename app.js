32e983150f48569b53c534afe8e589310ae5c3f1
ayush <rajayush746@gmail.com>
2023-07-15 21:11:28 +0530

    vercel

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 const port = process.env.PORT;
 
-// const Project = require('./model/projectSchema');
+const Project = require('./model/projectSchema');
 
 app.get('/user_details', (req, res) => {
     
